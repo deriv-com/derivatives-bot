3e3631975d4e3268a1c027e13a0581b7b2c2c930
prince-deriv <82309725+prince-deriv@users.noreply.github.com>
2025-10-01 15:44:42 +0400

    Revert "chore: test gdrive in staging"

--- src/components/load-modal/load-modal.tsx
+++ src/components/load-modal/load-modal.tsx
@@ -10,7 +10,7 @@ import { LOAD_MODAL_TABS } from '../../analytics/utils';
 import MobileFullPageModal from '../shared_ui/mobile-full-page-modal';
 import Modal from '../shared_ui/modal';
 import Tabs from '../shared_ui/tabs';
-import GoogleDrive from './google-drive';
+// import GoogleDrive from './google-drive'; // Removed - Google Drive token not finalized
 import Local from './local';
 import LocalFooter from './local-footer';
 import Recent from './recent';
@@ -21,6 +21,7 @@ const LoadModal: React.FC = observer(() => {
     const {
         active_index,
         is_load_modal_open,
+        loaded_local_file,
         onEntered,
         recent_strategies,
         setActiveTabIndex,
@@ -64,7 +65,7 @@ const LoadModal: React.FC = observer(() => {
         );
     }
 
-    const is_file_loaded = tab_name === tabs_title.TAB_LOCAL;
+    const is_file_loaded = !!loaded_local_file && tab_name === tabs_title.TAB_LOCAL;
     const has_recent_strategies = recent_strategies.length > 0 && tab_name === tabs_title.TAB_RECENT;
 
     return (
@@ -91,9 +92,6 @@ const LoadModal: React.FC = observer(() => {
                     </div>
                     <div label={localize('Local')}>
                         <Local />
-                    </div>
-                    <div label={localize('Google Drive')}>
-                        <GoogleDrive />
                     </div>
                 </Tabs>
             </Modal.Body>
