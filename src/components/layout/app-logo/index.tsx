5e2f5f4aab92935a4fc2bf45c4f4ef78f4b64dea
Rupato Braganza <rupato@deriv.com>
2025-10-31 14:36:19 +0800

    fix: translations on quick startegy modal

--- src/components/layout/app-logo/index.tsx
+++ src/components/layout/app-logo/index.tsx
@@ -9,7 +9,7 @@ export const AppLogo = () => {
     // Always go to the new home dashboard, regardless of login state
     // Logo now shows on both desktop and mobile
     // Add language parameter to the URL
-    const logoUrl = currentLang ? `${standalone_routes.deriv_app}?lang=${currentLang.toLowerCase()}` : standalone_routes.deriv_app;
+    const logoUrl = currentLang ? `${standalone_routes.deriv_app}?lang=${currentLang.toUpperCase()}` : standalone_routes.deriv_app;
     
     return <DerivLogo className='app-header__logo' href={logoUrl} variant='wallets' />;
 };
